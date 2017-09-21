d6fc20ab2b6a5282c1d964ab66f8893cac0d3563
Shailendra Singh <shail.friendly@gmail.com>
2017-09-21 17:51:09 -0500

    Updated Permutations

--- src/problems/string/Permutations.java
+++ src/problems/string/Permutations.java
@@ -15,24 +15,34 @@ public class Permutations {
 			return permutations;
 		}
 		char firstChar = str.charAt(0);
+		System.out.println("firstChar:" + firstChar);
+		
 		String remainingString = str.substring(1);
+		System.out.println("remainingString:" + remainingString);
 
-		Set<String> permutationSet = getAllPermutations(remainingString);
+		Set<String> set = getAllPermutations(remainingString);
+		System.out.println("set:" + set);
 
-		for (String permutationSetEntry : permutationSet) {
-			for (int i = 0; i <= permutationSetEntry.length(); i++) {
-				String start = permutationSetEntry.substring(0, i);
-				String end = permutationSetEntry.substring(i);
-				String permutaion = start + firstChar + end;
-				permutations.add(permutaion);
+		for (String entry : set) {
+			for (int i = 0; i <= entry.length(); i++) {
+				String permutation = getSinglePermutation(entry, firstChar, i);
+				System.out.println("permutation:" + permutation);
+				permutations.add(permutation);
 			}
 		}
 		return permutations;
 	}
 
+	private static String getSinglePermutation(String str, char ch, int i) {
+		String start = str.substring(0, i);
+		String end = str.substring(i);
+		String permutaion = start + ch + end;
+		return permutaion;
+	}
+
 	public static void test() {
 		String str = "ABC";
 		Set<String> permutations = getAllPermutations(str);
-		System.out.println(Arrays.toString(permutations.toArray()));
+		System.out.println("permutations:"+Arrays.toString(permutations.toArray()));
 	}
 }
