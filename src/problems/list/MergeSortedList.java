bc791eefa098e195e791233f4efb88e9195be4ba
Singh <SS8468@zebra.com>
2018-09-12 20:05:44 +0530

    MergeSortedList.java 12 Sep 8PM

--- src/problems/list/MergeSortedList.java
+++ src/problems/list/MergeSortedList.java
@@ -4,38 +4,37 @@ import data_structures.Node;
 
 public class MergeSortedList {
 
-	private static Node result;
 	private static Node a, b;
 
-	private static Node getSortedNode(Node a, Node b) {
-		System.out.println("a:" + a + ",b:" + b);
-		if (null == a || null == b) {
-			if (null != a) {
-				return b;
+	private static Node getResult(Node headA, Node headB) {
+		if (headA == null)
+			return headB;
+		if (headB == null)
+			return headA;
+
+		Node head = headA.mData < headB.mData ? headA : headB;
+
+		Node currA = headA, currB = headB;
+		while (currA != null && currB != null) {
+			if (currA.mData < currB.mData) {
+				Node aNext = currA.next;
+				if (aNext == null || aNext.mData > currB.mData) {
+					currA.next = currB;
+				}
+				currA = aNext;
+			} else {
+				Node bNext = currB.next;
+				if (bNext == null || bNext.mData > currA.mData) {
+					currB.next = currA;
+				}
+				currB = bNext;
 			}
-			return a;
 		}
-		if (a.mData <= b.mData) {
-			a.next = b;
-			return a;
-		} else if (b.mData <= a.mData) {
-			b.next = a;
-			return b;
-		}
-		return null;
+
+		return head;
 	}
 
-	private static Node getResult(Node a, Node b) {
-		if (null == result) {
-			if (a.mData <= b.mData) {
-				result = a;
-				result.next = b;
-			} else if (b.mData <= a.mData) {
-				result = b;
-				result.next = a;
-			}
-		}
-
+	private static void printResult(Node result) {
 		while (null != result) {
 			System.out.print(result);
 			result = result.next;
@@ -43,15 +42,9 @@ public class MergeSortedList {
 				System.out.print("->");
 			}
 		}
-
-		while (null != a.next && null != b.next) {
-			
-		}
-
-		return result;
 	}
 
-	private static void uploadData() {
+	public static void test() {
 		Node a1 = new Node(1);
 		Node a2 = new Node(2);
 		a1.next = a2;
@@ -63,11 +56,8 @@ public class MergeSortedList {
 		a11.next = a12;
 
 		b = a11;
-	}
 
-	public static void test() {
-		uploadData();
 		Node result = getResult(a, b);
-		// System.out.println("Result:" + result);
+		printResult(result);
 	}
 }
